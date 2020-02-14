4b4ce99aa91c3446902c19574e5467f057850e9c
AkaresQ <61059422+AkaresQ@users.noreply.github.com>
2020-02-14 23:05:54 +0200

    Update Request.py

--- Request.py
+++ Request.py
@@ -1,1 +1,12 @@
-import requests
+import requests , json 
+un = 'username' 
+pw = 'password'
+rapi = 'https://authserver.mojang.com/authenticate'
+requestsData = json.dumps({'agent': {'name': 'Minecraft','version': 1},'username': un,'password': pw})
+headers = {'Content-Type': 'application/json; charset=utf-8'}
+Sess = requests.post(rapi,requestsData,headers=headers)
+readJson = Sess.json()
+if 'accessToken' in readJson:
+  print("Valid")
+else:
+  print("Invalid")
